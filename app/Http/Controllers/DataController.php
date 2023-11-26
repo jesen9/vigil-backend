25c82b594c5d8ce0a8c2a1f5848599a286bb34e7
Jason Lionardi <jason.lionardi@binus.edu>
2023-11-26 22:54:53 +0700

    integrate nvd cve api

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -12,10 +12,49 @@ class DataController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-//        $breeds = Http::get('https://dog.ceo/api/breeds/list/random/5')['message'];
-        return Http::get('https://dog.ceo/api/breeds/list/random/5')['message'];
+//        dd($request->query->all()); // loop ini aja
+//        dd(count($request->query));
+        /*
+        $cpename = $request->query("cpeName");
+        $cveId = $request->query("cveId");
+        $cvssV3Metrics = $request->query("cvssV3Metrics");
+        $cvssV3Severity = $request->query("cvssV3Severity");
+        $cweId = $request->query("cweId");
+        $hasCertAlerts = $request->query("hasCertAlerts");
+        $hasCertNotes = $request->query("hasCertNotes");
+        $hasKev = $request->query("hasKev");
+        $hasOval = $request->query("hasOval");
+        $isVulnerable = $request->query("isVulnerable");
+        $keywordExactMatch = $request->query("keywordExactMatch");
+        $keywordSearch = $request->query("keywordSearch");
+        $virtualMatchString = $request->query("virtualMatchString");
+        $noRejected = $request->query("noRejected");
+        $resultsPerPage = $request->query("resultsPerPage");
+        $startIndex = $request->query("startIndex");
+        $sourceIdentifier = $request->query("sourceIdentifier");
+        */
+
+//        lastModStartDate & lastModEndDate
+//        pubStartDate & pubEndDate
+//        versionEnd & versionEndType
+//        versionStart & versionStartType
+
+        $baseurl = "https://services.nvd.nist.gov/rest/json/cves/2.0";
+        $counter = 0;
+
+        foreach($request->query->all() as $param => $value)
+        {
+//            if($counter++ == 0) continue;
+            $baseurl .= ($counter++ == 0 ? "?" : "&") . $param . "=" . $value;
+        }
+
+//        dd($baseurl);
+        dd(Http::get($baseurl));
+
+//        return Http::get('https://dog.ceo/api/breeds/list/random/5')['message'];
+        return Http::get($baseurl);
 //        dd($breeds);
         //return view('index',compact('breeds'));
     }

534e8e60d3f699ddc46c42ccb4583a011d0a4fc7
LAPTOP-C52HS6IE\jason <jason.lionardi@binus.ac.id>
2024-01-19 14:22:01 +0700

    define api auth guard in config/auth.php, modify routes for login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DataController;
 use App\Http\Controllers\NotesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,12 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/login', [UserController::class, 'login']);
 Route::get('/get-cve-list', [DataController::class, 'getCveList']);
 Route::get('/get-cve-details', [DataController::class, 'getCveDetails']);
 Route::get('/update-database', [DataController::class, 'updateDatabase']);
-Route::post('/insert-notes', [DataController::class, 'insertNotes']);
-Route::get('/get-notes', [DataController::class, 'getNotes']);
-Route::delete('/delete-notes/{id}', [DataController::class, 'deleteNotes']);
+
+
+Route::middleware(['auth:api'])->group(function(){
+    Route::post('/insert-notes', [DataController::class, 'insertNotes']);
+    Route::get('/get-notes', [DataController::class, 'getNotes']);
+    Route::delete('/delete-notes/{id}', [DataController::class, 'deleteNotes']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
